c7ef3a7b9a5144a5b7a9bf4c047bddf21a4a0001
seungjohan <cmostrillion@gmail.com>
2021-07-26 21:48:18 +0900

    library re n request

---
+++ inflearn-scraping/crawling.py
@@ -0,0 +1,8 @@
+# from urllib.request import urlopen
+# from bs4 import BeautifulSoup
+
+# html = urlopen("http://www.google.com")
+# bsObject = BeautifulSoup(html, "html.parser")
+
+# for meta in bsObject.head.find_all('meta'):
+#     print(meta.get('content'))
