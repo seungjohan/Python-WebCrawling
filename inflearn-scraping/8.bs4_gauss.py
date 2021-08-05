768e9e3bfa453085c3e50f16b2e412ee8ff0d6d6
seungjohan <cmostrillion@gmail.com>
2021-08-05 17:52:09 +0900

    bs4 imtermediate with naver webtoon

--- inflearn-scraping/8.bs4_gauss.py
+++ inflearn-scraping/8.bs4_gauss.py
@@ -14,8 +14,22 @@ cartoons = soup.find_all("td", attrs={"class":"title"})
 # print(title)
 # print("https://comic.naver.com" + link)
 
-
+# 만화 제목과 링크 가져오기
 for cartoon in cartoons:
     title = cartoon.a.get_text()
     link = "https://comic.naver.com" + cartoon.a["href"]
     print(title, link)
+
+
+# 별점을 가지고 평균 평점 내보기
+total_rates = 0
+
+cartoonratings = soup.find_all("div", attrs={"class":"rating_type"})
+
+for cartoonrating in cartoonratings:
+    rate = cartoonrating.find("strong").get_text()
+    print(rate)
+    total_rates += float(rate)
+
+print("total_score", total_rates)
+print("median_score", total_rates / len(cartoonratings))
