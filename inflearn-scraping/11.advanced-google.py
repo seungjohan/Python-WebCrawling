9d799ec00e9692a02d4e08aa38be5f21d591c87b
seungjohan <cmostrillion@gmail.com>
2021-08-17 23:26:51 +0900

    advanced-google

---
+++ inflearn-scraping/11.advanced-google.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+res = requests.get("https://www.google.com/search?sxsrf=ALeKk01zreiakgUn68k-YghojJtm3QBZXQ:1628677306134&q=showtimes&stick=H4sIAAAAAAAAAOPg3cDI-IhRnlvg5Y97wlJik9acvMYowMXnm1-WmRqckV9ekpmbWsyziJWzGMYBAK98tN0zAAAA&npsic=0&sxsrf=ALeKk01zreiakgUn68k-YghojJtm3QBZXQ:1628677306134&ved=2ahUKEwjemaTi36jyAhWPfd4KHf5bDmsQ0CsoADAUegQIARAy")
+res.raise_for_status
+
+soup = BeautifulSoup(res.text, "lxml")
+
+images=soup.find_all("g-img", attrs={"class": "BA0A6c"})
+
+
+for image in images:
+    print(images["src"])
+
+    image_url = image["src"]
+
+    if image_url.startswith("//"):
+        image_url = "https" + image_url
+
+
+
+
+# switch문으로
