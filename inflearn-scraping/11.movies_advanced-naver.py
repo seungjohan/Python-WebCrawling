aa20db6b434462e42dd0e5b46d2ddaf19eae94d9
seungjohan <cmostrillion@gmail.com>
2021-08-21 17:35:01 +0900

    movie advanced with movie ranking in naver

--- inflearn-scraping/11.movies_advanced-naver.py
+++ inflearn-scraping/11.movies_advanced-naver.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-for year in range(2012, 2021) :
+for year in range(2012, 2022) :
     url = "https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&qvt=0&query={}%EB%85%84%20%EC%98%81%ED%99%94%20%EC%88%9C%EC%9C%84".format(year)
 
     res = requests.get(url)
@@ -10,11 +10,26 @@ for year in range(2012, 2021) :
 
     soup = BeautifulSoup(res.text, "lxml")
 
-    images = soup.find_all("img", attrs={"class":"thum_msk"})
+    # images = soup.find_all("img", attrs={"class":"thum_msk"})
+    img_tag = soup.select_one('div._content')
+
+    images = img_tag.select('ul > li > div > a > img')
 
     for idx, image in enumerate(images):
         image_url = image["src"]
-        print(image["src"])
 
         if image_url.startswith("//"):
             image_url = "https" + image_url
+
+        print(image["src"])
+
+        image_res = requests.get(image_url)
+        image_res.raise_for_status()
+
+        with open("movie_{}_{}.jpg".format(year, idx + 1), "wb") as f:
+            f.write(image_res.content)
+
+        if idx >= 7:
+            break
+
+
